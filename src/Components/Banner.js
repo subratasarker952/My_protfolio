bda8ed7313ae6498002d0815de8e937b035b5daa
Subrata Sarkar <tccdcc8@gmail.com>
2024-07-03 22:51:32 +0600

    m

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,5 +1,5 @@
 import React from "react";
-import banner from "../Images/baner.png";
+import banner from "../Images/About.png";
 import { Button } from "react-bootstrap";
 
 const Banner = () => {
@@ -11,13 +11,13 @@ const Banner = () => {
             <div className="p-5">
               <h1 className="big">
                 Hello !<br /> I am{" "}
-                <span className="salmon">Subrata Sarker </span>{" "}
+                <span className="salmon">Subrata Sarker </span>
               </h1>
               <h1 className="big">Full-Stack Web Developer. </h1>
             </div>
             <a
               target="download"
-              href="https://drive.google.com/file/d/1w-jRaWWMQMOshtitOPovzbl2praBwOlW/view?usp=sharing"
+              href="https://drive.google.com/file/d/1_EpWP2313rOU-Er3yTodThs37oX3FCiY/view"
               rel="noreferrer"
               className="warning"
             >
